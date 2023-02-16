ba7c21fc7df0f12ee2e0466d3656b51681fac016
RATIU5 <jcmemmott20@outlook.com>
2023-02-16 10:43:06 -0700

    rewrite: updated functionality of the game to adapt to the new format with arrays

--- src/main.ts
+++ src/main.ts
@@ -3,71 +3,73 @@ import { runGame } from "./lib";
 import "./style.css";
 
 const state = {
-	deck: new Deck(["player", "computer", "table"]),
-	playerCardCount: 25,
-	computerCardCount: 25,
+	cardDeck: new Deck(),
+	playerDeck: new Deck([]),
+	computerDeck: new Deck([]),
+	tableDeck: new Deck([]),
+	winner: "",
 };
 
 function setup() {
-	state.deck.shuffle();
-	state.deck.deal([
-		{ ownerIndex: 0, count: 26 }, // player
-		{ ownerIndex: 1, count: 26 }, // computer
-	]);
+	state.cardDeck.shuffle();
+	const playerCards = state.cardDeck.deal(26);
+	const computerCards = state.cardDeck.deal(26);
+	state.playerDeck.pushCards(playerCards);
+	state.computerDeck.pushCards(computerCards);
 }
 
 function playCards(playerNthCard: number, computerNthCard: number) {
-	const pIndex = state.deck.getNthIndexByOwner(playerNthCard, 0);
-	const cIndex = state.deck.getNthIndexByOwner(computerNthCard, 1);
-	if (pIndex === -1 || cIndex === -1) {
-		console.error(`Cannot find card at index ${pIndex} or ${cIndex}`);
+	const playerCard = state.playerDeck.pullCardAtIndex(playerNthCard);
+	const computerCard = state.computerDeck.pullCardAtIndex(computerNthCard);
+
+	if (playerCard === null || computerCard === null) {
+		console.error("Player or computer has no cards");
 		return;
 	}
 
-	console.log(`Player plays ${state.deck.cardAt(pIndex).toString()}`);
+	if (playerCard.isHigherThan(computerCard)) {
+		if (state.tableDeck.size > 0) {
+			const tableCards = state.tableDeck.pullAllCards();
+			state.playerDeck.pushCards(tableCards);
+		}
+		state.playerDeck.pushCards([playerCard, computerCard]);
+		if (state.computerDeck.size === 0) {
+			console.log("Player wins!");
+			state.winner = "Player";
+			return;
+		}
 
-	if (state.deck.cardAt(pIndex).isHigherThan(state.deck.cardAt(cIndex))) {
-		moveTableCards(0);
-		state.deck.cardAt(cIndex).setOwnerIndex(0);
-		if (cIndex > pIndex) {
-			state.deck.moveToEnd(cIndex);
-			state.deck.moveToEnd(pIndex);
-		} else {
-			state.deck.moveToEnd(pIndex);
-			state.deck.moveToEnd(cIndex);
+		console.log(`Player wins with ${playerCard.toString()} over ${computerCard.toString()}`);
+		console.log(state.playerDeck);
+	} else if (playerCard.isLowerThan(computerCard)) {
+		if (state.tableDeck.size > 0) {
+			const tableCards = state.tableDeck.pullAllCards();
+			state.computerDeck.pushCards(tableCards);
 		}
-		state.playerCardCount = state.deck.getAllCardsByOwner(0).length;
-	} else if (state.deck.cardAt(pIndex).isLowerThan(state.deck.cardAt(cIndex))) {
-		moveTableCards(1);
-		state.deck.cardAt(pIndex).setOwnerIndex(1);
-		if (cIndex > pIndex) {
-			state.deck.moveToEnd(cIndex);
-			state.deck.moveToEnd(pIndex);
-		} else {
-			state.deck.moveToEnd(pIndex);
-			state.deck.moveToEnd(cIndex);
+		state.computerDeck.pushCards([playerCard, computerCard]);
+		if (state.playerDeck.size === 0) {
+			console.log("Computer wins!");
+			state.winner = "Computer";
+			return;
 		}
-		state.computerCardCount = state.deck.getAllCardsByOwner(1).length;
+		console.log(`Computer wins with ${computerCard.toString()} over ${playerCard.toString()}`);
+		console.log(state.computerDeck);
 	} else {
-		state.deck.cardAt(pIndex).setOwnerIndex(2);
-		state.deck.cardAt(cIndex).setOwnerIndex(2);
-		playCards(playerNthCard + 1, computerNthCard + 1);
+		state.tableDeck.pushCards([playerCard, computerCard]);
+		console.log("Tie");
+		if (state.playerDeck.size === 0 || state.computerDeck.size === 0) {
+			console.log("Game over");
+			state.winner = "Tie";
+			return;
+		}
+		console.log(state.tableDeck);
 	}
-}
-
-function moveTableCards(ownerIndex: number) {
-	return state.deck.getAllCardsByOwner(2).forEach((card) => {
-		card.setOwnerIndex(ownerIndex);
-	});
 }
 
 function onCardClick(nthCard: number) {
-	const computerCardIndex = state.deck.getNthIndexByOwner(0, 1);
-	if (computerCardIndex === -1) {
-		console.error("Computer has no cards");
-		return;
+	if (state.winner === "") {
+		playCards(nthCard, 0);
 	}
-	playCards(nthCard, 1);
 }
 
 runGame(state, setup, onCardClick);
